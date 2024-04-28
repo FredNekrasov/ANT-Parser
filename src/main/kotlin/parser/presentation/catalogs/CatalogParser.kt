1a90aa105eb17547b4ffc6d84b261e80d5e62bf1
FredNekrasov <frednekrasov49@gmail.com>
2024-04-28 12:40:26 +0300

    Creating a CatalogParser that parses catalogs from the site

---
+++ src/main/kotlin/parser/presentation/catalogs/CatalogParser.kt
@@ -0,0 +1,15 @@
+package parser.presentation.catalogs
+
+import org.jsoup.Jsoup
+import javax.inject.Inject
+
+class CatalogParser @Inject constructor() {
+    fun parseCatalogs(
+        mainPage: String = "https://hramalnevskogo.ru/", clazzName: String = "t451m__list_item"
+    ): List<String> = Jsoup.connect(mainPage).get()
+        .getElementsByClass(clazzName)
+        .html()
+        .replace("<a[\\w\\s-.\"=/>]*".toRegex(), "")
+        .split("</a>")
+        .dropLastWhile { it.isBlank() }
+}
