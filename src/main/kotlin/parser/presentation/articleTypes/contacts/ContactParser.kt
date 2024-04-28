d440887cc1cf9787c51e9c838214244cc5985330
FredNekrasov <frednekrasov49@gmail.com>
2024-04-28 12:41:17 +0300

    Creating a ContactParser class that parses contacts from the site

---
+++ src/main/kotlin/parser/presentation/articleTypes/contacts/ContactParser.kt
@@ -0,0 +1,30 @@
+package parser.presentation.articleTypes.contacts
+
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+
+class ContactParser {
+    private fun parseData(
+        contactPage: String = "https://hramalnevskogo.ru/page42046286.html"
+    ): Document = Jsoup.connect(contactPage).get()
+    fun parseTitle(clazzName: String = "t555__contentwrapper"): String = parseData()
+        .getElementsByClass(clazzName)
+        .html()
+        .split("<[\\w>\\s=\"-:;!@]*".toRegex())
+        .filter { it.isNotBlank() }
+        .slice(0..1)
+        .joinToString(" ") { it.trim() }
+    fun parseContacts(
+        clazzName: String = "t451__rightside t451__side t451__side_socials",
+        tag: String = "a",
+        attribute: String = "href"
+    ): String = parseData()
+        .getElementsByClass(clazzName)
+        .map {
+            it.getElementsByTag(tag)
+        }
+        .flatten()
+        .joinToString("\n") {
+            it.attr(attribute).trim()
+        }
+}
