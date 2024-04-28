7e7992cd64ea8697ed0c8ce43348e201cbe20c6e
FredNekrasov <frednekrasov49@gmail.com>
2024-04-28 12:41:56 +0300

    Creating a MainPageParser class that parses main page of the site

---
+++ src/main/kotlin/parser/presentation/articleTypes/main/MainPageParser.kt
@@ -0,0 +1,35 @@
+package parser.presentation.articleTypes.main
+
+import org.jsoup.Jsoup
+import org.jsoup.select.Elements
+
+class MainPageParser {
+    private val mainPage: String = "https://hramalnevskogo.ru/"
+    private val clazzName: String = "tn-atom"
+    private fun parseData(): Elements = Jsoup.connect(mainPage)
+        .get()
+        .getElementsByClass(clazzName)
+    fun parseBanner(): String = parseData()
+        .map {
+            it.wholeOwnText()
+                .replace("\\s\\s+".toRegex(), "")
+                .trim()
+        }
+        .slice(0..1)
+        .joinToString("")
+        .split("\n")
+        .joinToString(" ")
+    private fun parseText(): List<String> = parseData()
+        .html()
+        .replace("</?\\w*>".toRegex(), "\n")
+        .replace("\\s\\s+".toRegex(), "\n")
+        .split("\n".toRegex())
+    fun parseTitle(): String = parseText()
+        .slice(8..8)
+        .joinToString("")
+    fun parseDescription(): String = parseText()
+        .run {
+            slice(9..lastIndex)
+                .joinToString("\n")
+        }
+}
