044b8e6e0c3f1560984afdec0ec89df74021e37b
FredNekrasov <frednekrasov49@gmail.com>
2024-04-28 11:14:38 +0300

    Creating a class that gives us HttpClient

---
+++ src/main/kotlin/parser/data/remote/WebAPI.kt
@@ -0,0 +1,9 @@
+package parser.data.remote
+
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+
+class WebAPI {
+    private val httpClient = HttpClient(CIO)
+    fun getHttpClient(): HttpClient = httpClient
+}
