2aa9cd99aceb3bf1dafb1793bd673bec2e6c8ff3
FredNekrasov <frednekrasov49@gmail.com>
2024-05-04 12:37:43 +0300

    koin testing in the main function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,13 @@
-import org.jsoup.Jsoup
+import di.*
+import org.koin.core.context.startKoin
+import parser.catalogs.CatalogParser
 
 fun main() {
-    println(foo())
+    val app = startKoin {
+        printLogger()
+        modules(dataLayer, domainLayer, presentationLayer)
+    }
+    app.koin.get<CatalogParser>().parseCatalogs().joinToString("\n") { it.trim() }.let { println(it) }
 }
 // information https://hramalnevskogo.ru/page42533272.html
 // spirit talk https://hramalnevskogo.ru/page40967215.html
-fun foo(
-    mainPage: String = "https://hramalnevskogo.ru/",
-    clazzName: String = "tn-atom"
-) = Jsoup.connect(mainPage)
-    .get()
-    .getElementsByClass(clazzName)
-    .html()
-    .replace("</?\\w*>".toRegex(), "\n")
-    .replace("\\s\\s+".toRegex(), "\n")
-    .split("\n".toRegex())
-    .let {
-        it.slice(8..it.lastIndex)
-    }
-    .joinToString("\n")
