739666485e7113b03091776f031d1cd6db7122cb
FredNekrasov <frednekrasov49@gmail.com>
2024-05-05 19:31:29 +0300

    removing an unnecessary WebAPI.kt class

--- src/main/kotlin/data/remote/WebAPI.kt
+++
@@ -1,9 +0,0 @@
-package data.remote
-
-import io.ktor.client.*
-import io.ktor.client.engine.cio.*
-
-class WebAPI {
-    private val httpClient = HttpClient(CIO)
-    fun getHttpClient(): HttpClient = httpClient
-}
