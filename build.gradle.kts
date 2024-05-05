58e2ade1b5f759e0af9b6315f4a0eba318c8e7da
FredNekrasov <frednekrasov49@gmail.com>
2024-05-05 19:28:46 +0300

    adding the necessary dependencies to work with the api

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,10 @@ dependencies {
     implementation("io.ktor:ktor-client-cio:2.3.10")
     implementation("io.ktor:ktor-client-logging:2.3.10")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
+    // serialization
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+    implementation("io.ktor:ktor-client-content-negotiation:2.3.10")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.10")
     // html parser
     implementation("org.jsoup:jsoup:1.17.2")
     // DI
