50fbf442ff9248607ce60c6a766d697bf856dd83
FredNekrasov <frednekrasov49@gmail.com>
2024-05-08 20:19:03 +0300

    creating an object with strings

---
+++ src/main/kotlin/presentation/utils/Strings.kt
@@ -0,0 +1,17 @@
+package presentation.utils
+
+object Strings {
+    // actions
+    const val ACTIONS = "\n Select an action:\n 1. Catalogs\n 2. Articles\n 3. Finish the job"
+    const val SUB_ACTIONS = "\n Select an action:\n 1. Display data from server\n 2. Display parsed data\n 3. Add data\n 3. Delete\n 5. Exit"
+    // dialogs
+    const val CONFIRM = "Are you sure you want to <do something>? Yes/No"
+    const val SEARCH = "Choose which field to search by:"
+    const val INDEX = "Enter the index of the record"
+    // errors
+    const val INDEX_ERROR = "Error: this index doesn't exist"
+    const val NULL = "Error: Value is null"
+    const val ERROR = "Error: Value is null or incorrect"
+    const val INCORRECT_CHOICE = "Error: There is no such choice"
+    const val WRONG_CHOICE = "Error: Make the right choice"
+}
