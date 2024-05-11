5a96b333e1ea04dc448c3ae009143bda2eccf197
FredNekrasov <frednekrasov49@gmail.com>
2024-05-11 08:38:05 +0300

    creating a DIStrings object to use qualifiers

---
+++ src/main/kotlin/di/utils/DIStrings.kt
@@ -0,0 +1,13 @@
+package di.utils
+
+object DIStrings {
+    // data
+    const val CATALOG_REPOSITORY = "catalogRepository"
+    const val ARTICLE_REPOSITORY = "articleRepository"
+    const val CONTENT_REPOSITORY = "contentRepository"
+    const val HTTP_CLIENT = "http client"
+    // domain
+    const val CATALOG_INTERACTORS = "catalogInteractors"
+    const val ARTICLE_INTERACTORS = "articleInteractors"
+    const val CONTENT_INTERACTORS = "contentInteractors"
+}
